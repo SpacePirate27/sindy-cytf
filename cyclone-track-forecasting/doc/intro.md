b3a6f7b34e397bd866a9fd0a184491b2c48e4503
SpacePirate27 <57584367+SpacePirate27@users.noreply.github.com>
2025-03-11 23:00:57 +0530

    setup clojure project

---
+++ cyclone-track-forecasting/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cyclone-track-forecasting
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cyclone-track-forecasting/test/cyclone_track_forecasting/core_test.clj
@@ -0,0 +1,7 @@
+(ns cyclone-track-forecasting.core-test
+  (:require [clojure.test :refer :all]
+            [cyclone-track-forecasting.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
