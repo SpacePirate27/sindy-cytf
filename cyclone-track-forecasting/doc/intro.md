28b9908c85554100b7b3d38037799522a9c9659b
SpacePirate27 <57584367+SpacePirate27@users.noreply.github.com>
2025-03-11 23:32:49 +0530

    clean up

--- cyclone-track-forecasting/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to cyclone-track-forecasting
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- cyclone-track-forecasting/src/cyclone_track_forecasting/core.clj
+++ cyclone-track-forecasting/src/cyclone_track_forecasting/core.clj
@@ -1,7 +1,3 @@
 (ns cyclone-track-forecasting.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
