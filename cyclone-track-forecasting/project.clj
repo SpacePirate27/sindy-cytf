f2d138c295e517e7a4bf87cc42eb3423e93963f9
naimish240 <naimish240@gmail.com>
2025-03-15 09:48:03 +0530

    Added changes

--- cyclone-track-forecasting/project.clj
+++ cyclone-track-forecasting/project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojars.haifengl/smile "4.2.0"]
+                 [org.scicloj/scicloj.ml.xgboost "6.3.0"]
+                 [org.scicloj/metamorph.ml "1.2"]
+                 [techascent/tech.ml.dataset "7.052"]]
   :main ^:skip-aot cyclone-track-forecasting.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
