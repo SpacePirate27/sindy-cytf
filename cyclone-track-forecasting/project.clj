0f6cb98713f405d4c23f056fa6885541f24c98f5
naimish240 <naimish240@gmail.com>
2025-03-15 18:28:20 +0530

    got model working

--- cyclone-track-forecasting/project.clj
+++ cyclone-track-forecasting/project.clj
@@ -4,12 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojars.haifengl/smile "4.2.0"]
-                 [org.scicloj/scicloj.ml.xgboost "6.3.0"]
-                 [org.scicloj/metamorph.ml "1.2"]
-                 [org.scicloj/clay "2-beta32"]
-                 [scicloj/tablecloth "7.042"]
-                 [org.scicloj/kindly "4-beta15"]]
+                 [org.scicloj/noj "2-beta10.1"]
+                 [org.scicloj/scicloj.ml.smile "7.5.2"]
+                 [org.scicloj/clay "2-beta33"]]
   :main ^:skip-aot cyclone-track-forecasting.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- cyclone-track-forecasting/src/cyclone_track_forecasting/core.clj
+++ cyclone-track-forecasting/src/cyclone_track_forecasting/core.clj
@@ -1,12 +1,48 @@
 (ns cyclone-track-forecasting.core
   (:require [scicloj.kindly.v4.kind :as kind]
-            [tablecloth.api :as tc])
+            [tablecloth.api :as tc]
+            [tech.v3.dataset :as ds]
+            [tech.v3.dataset.metamorph :as ds-mm]
+            [scicloj.metamorph.core :as mm]
+            [scicloj.metamorph.ml :as ml]
+            [scicloj.metamorph.ml.loss :as loss]
+            [scicloj.ml.smile.regression])
   )
 
-; # This is a test
 
-(defn load-dataset 
-  "Loads the dataset"
-  [path]
-  (tc/dataset path)
-  )
+
+; ### Load and process Dataset
+
+(def ds (ds/->dataset "resources/final_dataset.csv" {:key-fn keyword}))
+
+(def ds-intermediate (tc/drop-columns ds [:name :basin :filename :timestamp]))
+(def ds-final (tc/drop-missing ds-intermediate))
+
+
+; ### Train-Test split
+(def split
+  (first
+   (tc/split->seq ds-final :holdout {:seed 112723})))
+
+(:train split)
+(:test split)
+
+; ### Pipeline
+(def ols-pipe-fn
+  (mm/pipeline
+   (ds-mm/set-inference-target :dx)
+   #:metamorph{:id :model}
+   (ml/model {:model-type :smile.regression/ordinary-least-square})))
+
+
+; ### Fit Regressor
+(def fitted (mm/fit (:train split)  ols-pipe-fn))
+
+; ### Get Prediction
+(def prediction
+  (-> (:test split)
+      (mm/transform-pipe ols-pipe-fn fitted)
+      :metamorph/data
+      :dx))
+
+prediction
